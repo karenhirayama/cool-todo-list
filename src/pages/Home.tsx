b1b946f74f0038a2e050b7f5efc59ac23a337073
karenhirayama <karenm.hirayama@gmail.com>
2022-08-20 19:54:57 -0300

    feat: Add Home route

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Button } from '../components'
 
 export default function Home() {
+
+  const logIn = () => {
+console.log('clicked')
+  }
   return (
-    <div>Home</div>
+    <div>
+      <Button onClick={logIn} text={'Log in'} />
+    </div>
   )
 }
